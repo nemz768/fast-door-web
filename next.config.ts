527261f68687c9df32bcc9f04a9d10386f3c6e44
Pavel <pavel20042019@gmail.com>
2026-04-08 10:57:36 +0700

    docker support

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   productionBrowserSourceMaps: false, // отключаем source maps
+  output: "standalone", // для оптимизации сборки
 };
 
 export default nextConfig;
